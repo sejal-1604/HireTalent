60e1406f0fa35fb61febfbb855d8b600be40be4b
Sejal <sejalchn1624@gmail.com>
2025-07-04 14:24:33 +0530

    first commit

---
+++ backend/config/MongoDB.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+module.exports = connectDB;
