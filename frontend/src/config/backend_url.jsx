d13e20647313879fd9ba846107964533089cca38
sejal-1604 <sejalchn1624@gmail.com>
2025-09-27 19:38:01 +0530

    Update backend_url.jsx

--- frontend/src/config/backend_url.jsx
+++ frontend/src/config/backend_url.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://vercel.com/sejals-projects-c804ef4c/hire-talent';
+export const BASE_URL = 'https://hire-talent-iv4x.vercel.app/';
 // export const BASE_URL = 'https://cpms-api.vercel.app';
